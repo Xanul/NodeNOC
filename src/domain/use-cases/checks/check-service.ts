7cb7162bfa6e34288a5cdaab2f7f2b5fcbefea65
Xanul <rivas.060@gmail.com>
2024-02-29 13:54:43 -0600

    Added the use case to check for service

---
+++ src/domain/use-cases/checks/check-service.ts
@@ -0,0 +1,25 @@
+interface CheckServiceUseCase {
+  execute(url: string): Promise<boolean>;
+}
+
+export class CheckService implements CheckServiceUseCase {
+
+  public async execute(url: string):Promise<boolean> {
+
+    try {
+      
+      const req = await fetch(url);
+      if (!req.ok) throw new Error(`Error on check service on: ${url}`);
+
+      console.log(`Service running on: ${url}`);
+      return true;
+
+    } catch (error) {
+      console.log(`${error}`);
+      return false
+    }
+
+
+  }
+
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,29 +1,17 @@
 import { CronJob } from "cron";
 import { CronService } from "./cron/cron-service";
+import { CheckService } from "../domain/use-cases/checks/check-service";
 
 export class Server {
   public static start() {
     console.log("Server started...");
 
     CronService.createJob(
-      "*/5 * * * * *",
-      () => {
-        const date = new Date();
-        console.log("5 Seconds Cron Job", date);
-      }
-    );
-    CronService.createJob(
       "*/3 * * * * *",
       () => {
         const date = new Date();
-        console.log("3 Seconds Cron Job", date);
-      }
-    );
-    CronService.createJob(
-      "*/2 * * * * *",
-      () => {
-        const date = new Date();
-        console.log("2 Seconds Cron Job", date);
+        new CheckService().execute("https://www.google.com");
+        // new CheckService().execute("http://localhost:3000");
       }
     );
   }
